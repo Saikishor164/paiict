cdd0d298dca064d3c94e8c0697ccaee18419ec3c
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-09-18 13:06:47 +0530

    made form css

--- form/header.php
+++ form/header.php
@@ -2,30 +2,41 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<style>
+    img{
+    max-width: 100%; /* Make the images responsive */
+    height: auto; /* Maintain aspect ratio */
+                                         }
+
+
+</style>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="stylesheet.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+
+<img src="header.png" alt="header image">
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container">
-            <!-- Logo and Website Name -->
+             Logo and Website Name 
             <a class="navbar-brand" href="#">
                 <img src="http://localhost:8080/paiict\wp-content\uploads\2023\08\PAI-ROBO-CHAMPS-logo.png" class="img-fluid" width="30" height="30" alt="Logo">
                 PAI-ROBO CHAMP 2023
             </a>
             
-            <!-- Responsive Navbar Toggle Button -->
+             Responsive Navbar Toggle Button 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             
-            <!-- Navbar Links -->
+             Navbar Links
             <div class="collapse navbar-collapse" id="navbarNav">
             </div>
         </div>
-    </nav>
+    </nav> -->
     
     <!-- Include Bootstrap JS and jQuery if needed -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- form/index.php
+++ form/index.php
@@ -1,9 +1,9 @@
 <?php
      include_once('easebuzz-lib\easebuzz_payment_gateway.php');
      if(isset($_POST['submit'])){
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $phone = $_POST['phone'];
+      $name = $_POST['instadd'];
+      $email = $_POST['instemail'];
+      $phone = $_POST['principalno'];
       $amount = $_POST['amount'];
 
       $MERCHANT_KEY = "2PBP7IABZ2";  // S59212I6RX
@@ -47,7 +47,7 @@
 <html lang="en">
   <head>
     <title>PAI ROBO CHAMP</title>
-    <!-- Required meta tags -->
+    <!--   meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -83,101 +83,102 @@
       <div class="col-sm-12 form-group">
   <form action="" method="post">
   <label for="name">Name of the Institute<span>*</span></label>
-          <input class="form-control" type="text" placeholder="Name of the Institute" name="nameinst" id="nameinst" required><br>
+          <input class="input" type="text" placeholder="Name of the Institute" name="nameinst" id="nameinst"  ><br><br>
 
           <label for="instadd" class="label">Institute Address <span>*</span></label>
-          <input class="form-control" type="text" placeholder="Institute Address" name="instadd" id="instadd" required><br>
+          <input class="input" type="text" placeholder="Institute Address" name="instadd" id="instadd"  ><br><br>
 
           <label for="instemail">Institute Email <span>*</span></label>
-          <input class="form-control" type="email" placeholder="Institute Email" name="instemail" id="instemail" required><br>
+          <input class="input" type="email" placeholder="Institute Email" name="instemail" id="instemail"  ><br><br>
 
           <label for="principal">Principal Name<span>*</span></label>
-          <input class="form-control" tyepe="text" placeholder="Principal Name" id="principal" name="principal" required></input><br>
+          <input class="input" tyepe="text" placeholder="Principal Name" id="principal" name="principal"  ></input><br><br>
 
           <label for="principalno">Principal Contact Number <span>*</span></label>
-          <input type="tel" class="form-control" name="principalno" id="principalno" placeholder="Principal Contact Number" required><br>
+          <input type="number" class="input" name="principalno" id="principalno" placeholder="Principal Contact Number"  ><br><br>
 
           <label for="teamname">Team Name <span>*</span></label>
-          <input type="text" class="form-control" onkeypress="numonly(event)" placeholder="Phone No: " name="teamname" id="teamname" required><br>
+          <input type="text" class="input" onkeypress="numonly(event)" placeholder="Phone No: " name="teamname" id="teamname"  ><br><br>
 
           <label for="mentorname">Mentor Name<span>*</span></label>
-          <input type="email" class="form-control" placeholder="Mentor Name" name="mentorname" id="mentorname" required><br>
+          <input type="text" class="input" placeholder="Mentor Name" name="mentorname" id="mentorname"  ><br><br>
 
           <label for="mentorno">Mentor Contact Number</label>
-          <input type="tel" class="form-control" placeholder="Mentor Contact Number" name="mentorno" id="mentorno" required> <br>
+          <input type="number" class="input" placeholder="Mentor Contact Number" name="mentorno" id="mentorno"  > <br><br>
 
           <label for="mentorquali">Mentor Qualification</label>
-          <input type="text" class="form-control" placeholder="Mentor Qualification" name="mentorquali" id="mentorquali" required> <br>
+          <input type="text" class="input" placeholder="Mentor Qualification" name="mentorquali" id="mentorquali"  > <br><br>
 
           <label for="mentoradhar">mentor Adhar (200kb File)</label>
-          <input type="file" class="form-control" placeholder="mentor Adhar (200kb File)" name="mentoradhar" id="mentoradhar" required> <br>
+          <input type="file" class="input" placeholder="mentor Adhar (200kb File)" name="mentoradhar" id="mentoradhar"  > <br><br>
 
           <label for="stdfirstna">Teammate Members Details <br> 1) </label>
-          <input type="text" class="form-control" placeholder="Name of first Student" name="stdfirstna" id="stdfirstna" required> <br>
+          <input type="text" class="input" placeholder="Name of first Student" name="stdfirstna" id="stdfirstna"  > <br><br>
 
           <label for="stdfirstph"> Student Photo (200kb File)</label>
-          <input type="file" class="form-control" placeholder="student Photot" name="stdfirstph" id="stdfirstph" required> <br>
+          <input type="file" class="input" placeholder="student Photot" name="stdfirstph" id="stdfirstph"  > <br><br>
 
           <label for="stdagefirst"> Student Age</label>
-          <input type="number" class="form-control" placeholder="Student Age" name="stdagefirst" id="stdagefirst" required> <br>
+          <input type="number" maxlength="2" minlength="1" class="input" placeholder="Student Age" name="stdagefirst" id="stdagefirst"  > <br> <br>
 
           <label for="stddivfirst"> Student Class Division</label>
-          <input type="text" class="form-control" placeholder="Student Class Division" name="stddivfirst" id="stddivfirst" required> <br>
+          <input type="text" class="input" placeholder="Student Class Division" name="stddivfirst" id="stddivfirst"  > <br> <br>
 
 
           <label for="stdsecna">2) <br> Name of the Student  </label>
-          <input type="text" class="form-control" placeholder="Name of first Student" name="stdfirstdsecnastna" id="stdsecna" required> <br>
+          <input type="text" class="input" placeholder="Name of first Student" name="stdfirstdsecnastna" id="stdsecna"  > <br> <br>
 
           <label for="stdsecph"> Student Photo (200kb File)</label>
-          <input type="file" class="form-control" placeholder="student Photot" name="stdsecph" id="stdsecph" required> <br>
+          <input type="file" class="input" placeholder="student Photot" name="stdsecph" id="stdsecph"  > <br><br>
 
           <label for="stdagesec"> Student Age</label>
-          <input type="number" class="form-control" placeholder="Student Age" name="stdagesec" id="stdagesec" required> <br>
+          <input type="number" class="input" placeholder="Student Age" name="stdagesec" id="stdagesec"  > <br> <br>
 
           <label for="stddivsec"> Student Class Division</label>
-          <input type="text" class="form-control" placeholder="Student Class Division" name="stddivsec" id="stddivsec" required> <br>
+          <input type="text" class="input" placeholder="Student Class Division" name="stddivsec" id="stddivsec"  > <br> <br>
 
           <label for="stdthridna">3) <br> Name of the student </label>
-          <input type="text" class="form-control" placeholder="Name of first Student" name="stdthridna" id="stdthridna" required> <br>
+          <input type="text" class="input" placeholder="Name of first Student" name="stdthridna" id="stdthridna"  > <br> <br>
 
           <label for="stdthridph"> Student Photo (200kb File)</label>
-          <input type="file" class="form-control" placeholder="student Photot" name="stdthridph" id="stdthridph" required> <br>
+          <input type="file" class="input" placeholder="student Photot" name="stdthridph" id="stdthridph"  > <br> <br>
 
           <label for="stdagethrid"> Student Age</label>
-          <input type="number" class="form-control" placeholder="Student Age" name="stdagethrid" id="stdagethrid" required> <br>
+          <input type="number" class="input" placeholder="Student Age" name="stdagethrid" id="stdagethrid"  > <br> <br>
 
           <label for="stddivthrid"> Student Class Division</label>
-          <input type="text" class="form-control" placeholder="Student Class Division" name="stddivthrid" id="stddivthrid" required> <br>
+          <input type="text" class="input" placeholder="Student Class Division" name="stddivthrid" id="stddivthrid"  > <br> <br>
 
 
           <label for="stdfourtna">4) <br> Name of the Student  </label>
-          <input type="text" class="form-control" placeholder="Name of first Student" name="stdfourtna" id="stdfourtna" required> <br>
+          <input type="text" class="input" placeholder="Name of first Student" name="stdfourtna" id="stdfourtna"  > <br> <br>
 
           <label for="stdfourtph"> Student Photo (200kb File)</label>
-          <input type="file" class="form-control" placeholder="student Photo" name="stdfourtph" id="stdfourtph" required> <br>
+          <input type="file" class="input" placeholder="student Photo" name="stdfourtph" id="stdfourtph"  > <br> <br>
 
           <label for="stdagefourt"> Student Age</label>
-          <input type="number" class="form-control" placeholder="Student Age" name="stdagefourt" id="stdagefourt" required> <br>
+          <input type="number" class="input" placeholder="Student Age" name="stdagefourt" id="stdagefourt"  > <br> <br>
 
           <label for="stddivfourt"> Student Class Division</label>
-          <input type="text" class="form-control" placeholder="Student Class Division" name="stddivfourt" id="stddivfourt" required> <br>
+          <input type="text" class="input" placeholder="Student Class Division" name="stddivfourt" id="stddivfourt"  > <br> <br>
 
           <label for="stdfifthna">5) <br> Name of the Student  </label>
-          <input type="text" class="form-control" placeholder="Name of first Student" name="stdfifthna" id="stdfifthna" required> <br>
+          <input type="text" class="input" placeholder="Name of first Student" name="stdfifthna" id="stdfifthna"  > <br> <br>
 
           <label for="stdfifthph"> Student Photo (200kb File)</label>
-          <input type="file" class="form-control" placeholder="student Photo" name="stdfifthph" id="stdfifthph" required> <br>
+          <input type="file" class="input" placeholder="student Photo" name="stdfifthph" id="stdfifthph"  > <br> <br>
 
           <label for="stdagefifth"> Student Age</label>
-          <input type="number" class="form-control" placeholder="Student Age" name="stdagefifth" id="stdagefifth" required> <br>
+          <input type="number" class="input" placeholder="Student Age" name="stdagefifth" id="stdagefifth"  > <br> <br>
 
           <label for="stddivfifth"> Student Class Division</label>
-          <input type="text" class="form-control" placeholder="Student Class Division" name="stddivfifth" id="stddivfifth" required> <br>
+          <input type="text" class="input" placeholder="Student Class Division" name="stddivfifth" id="stddivfifth"  > <br> <br>
 
     <label for="amount">Amount:</label>
-    <input class="form-control" type="number" name="amount" id="amount" value="2000" readonly>
-    <br>
-    <input class="btn btn-success me-md-2" type="submit" value="submit" name="submit">
+    <input class="input" type="number" name="amount" id="amount" value="2000" readonly>
+    <br><br><br>
+    <button value="submit" name="submit" type="submit">Submit</button>
+    <!-- <input class="btn btn-success me-md-2" type="submit" value="submit" name="submit"> -->
     <input class="btn btn-primary me-md-2" type="reset" value="reset" name="reset">
     </form>
 
