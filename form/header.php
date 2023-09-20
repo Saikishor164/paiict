55dc435eae9613f7d9fd9e7cda978b3c11e01e76
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-09-20 19:46:04 +0530

    updated header menu in the form

--- form/header.php
+++ form/header.php
@@ -7,8 +7,8 @@
     img{
     max-width: 100%; /* Make the images responsive */
     height: auto; /* Maintain aspect ratio */
-                                         }
-
+        }
+        /* Style the main menu container */
 
 </style>
     <!-- Include Bootstrap CSS -->
@@ -19,6 +19,226 @@
 <body>
 
 <img src="header.png" alt="header_image">
+<div class="ast-below-header-wrap">
+            <div
+              class="ast-below-header-bar ast-below-header site-header-focus-item"
+              data-section="section-below-header-builder"
+            >
+              <div
+                class="site-below-header-wrap ast-builder-grid-row-container site-header-focus-item ast-container"
+                data-section="section-below-header-builder"
+              >
+                <div
+                  class="ast-builder-grid-row ast-grid-center-col-layout-only ast-flex ast-grid-center-col-layout"
+                >
+                  <div
+                    class="site-header-below-section-center site-header-section ast-flex ast-grid-section-center"
+                  >
+                    <div
+                      class="ast-builder-menu-1 ast-builder-menu ast-flex ast-builder-menu-1-focus-item ast-builder-layout-element site-header-focus-item"
+                      data-section="section-hb-menu-1"
+                    >
+                      <div class="ast-main-header-bar-alignment">
+                        <div class="main-header-bar-navigation">
+                          <nav
+                            class="site-navigation ast-flex-grow-1 navigation-accessibility site-header-focus-item"
+                            id="primary-site-navigation-desktop"
+                            aria-label="Site Navigation"
+                            itemtype="https://schema.org/SiteNavigationElement"
+                            itemscope="itemscope"
+                          >
+                            <div class="main-navigation ast-inline-flex">
+                              <ul
+                                id="ast-hf-menu-1"
+                                class="main-header-menu ast-menu-shadow ast-nav-menu ast-flex submenu-with-border stack-on-mobile"
+                              >
+                                <li
+                                  id="menu-item-88"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-27 current_page_item menu-item-88"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/"
+                                    aria-current="page"
+                                    class="menu-link"
+                                    >Home</a
+                                  >
+                                </li>
+                                <li
+                                  id="menu-item-89"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-89"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/our-patrons/"
+                                    class="menu-link"
+                                    >Our Patrons</a
+                                  >
+                                </li>
+                                <li
+                                  id="menu-item-90"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-90"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/competition-brief/"
+                                    class="menu-link"
+                                    >Competition brief</a
+                                  >
+                                </li>
+                                <li
+                                  id="menu-item-230"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-230"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/competition-rules/"
+                                    class="menu-link"
+                                    >Competition Rules<span
+                                      role="application"
+                                      class="dropdown-menu-toggle ast-header-navigation-arrow"
+                                      tabindex="0"
+                                      aria-expanded="false"
+                                      aria-label="Menu Toggle"
+                                      ><span class="ast-icon icon-arrow"
+                                        ><svg
+                                          class="ast-arrow-svg"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                                          version="1.1"
+                                          x="0px"
+                                          y="0px"
+                                          width="26px"
+                                          height="16.043px"
+                                          viewBox="57 35.171 26 16.043"
+                                          enable-background="new 57 35.171 26 16.043"
+                                          xml:space="preserve"
+                                        >
+                                          <path
+                                            d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z"
+                                          ></path></svg></span></span></a
+                                  ><button
+                                    class="ast-menu-toggle"
+                                    aria-expanded="false"
+                                  >
+                                    <span class="screen-reader-text"
+                                      >Menu Toggle</span
+                                    ><span class="ast-icon icon-arrow"
+                                      ><svg
+                                        class="ast-arrow-svg"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        version="1.1"
+                                        x="0px"
+                                        y="0px"
+                                        width="26px"
+                                        height="16.043px"
+                                        viewBox="57 35.171 26 16.043"
+                                        enable-background="new 57 35.171 26 16.043"
+                                        xml:space="preserve"
+                                      >
+                                        <path
+                                          d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z"
+                                        ></path></svg
+                                    ></span>
+                                  </button>
+                                  <ul class="sub-menu">
+                                    <li
+                                      id="menu-item-91"
+                                      class="menu-item menu-item-type-post_type menu-item-object-page menu-item-91"
+                                    >
+                                      <a
+                                        href="https://www.ictacademypune.com/pairobochamps/rules-regulations/"
+                                        class="menu-link"
+                                        ><span class="ast-icon icon-arrow"
+                                          ><svg
+                                            class="ast-arrow-svg"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                                            version="1.1"
+                                            x="0px"
+                                            y="0px"
+                                            width="26px"
+                                            height="16.043px"
+                                            viewBox="57 35.171 26 16.043"
+                                            enable-background="new 57 35.171 26 16.043"
+                                            xml:space="preserve"
+                                          >
+                                            <path
+                                              d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z"
+                                            ></path></svg></span
+                                        >Rules &amp; Regulations</a
+                                      >
+                                    </li>
+                                    <li
+                                      id="menu-item-236"
+                                      class="menu-item menu-item-type-post_type menu-item-object-page menu-item-236"
+                                    >
+                                      <a
+                                        href="https://www.ictacademypune.com/pairobochamps/terms-and-conditions/"
+                                        class="menu-link"
+                                        ><span class="ast-icon icon-arrow"
+                                          ><svg
+                                            class="ast-arrow-svg"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                                            version="1.1"
+                                            x="0px"
+                                            y="0px"
+                                            width="26px"
+                                            height="16.043px"
+                                            viewBox="57 35.171 26 16.043"
+                                            enable-background="new 57 35.171 26 16.043"
+                                            xml:space="preserve"
+                                          >
+                                            <path
+                                              d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z"
+                                            ></path></svg></span
+                                        >Terms and Conditions</a
+                                      >
+                                    </li>
+                                  </ul>
+                                </li>
+                                <li
+                                  id="menu-item-92"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/prizes/"
+                                    class="menu-link"
+                                    >Prizes</a
+                                  >
+                                </li>
+                                <li
+                                  id="menu-item-301"
+                                  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-301"
+                                >
+                                  <a
+                                    href="https://www.ictacademypune.com/pairobochamps/gallery/"
+                                    class="menu-link"
+                                    >Gallery</a
+                                  >
+                                </li>
+                                <li
+                                  id="menu-item-195"
+                                  class="registration menu-item menu-item-type-custom menu-item-object-custom menu-item-195"
+                                >
+                                  <a
+                                    href="http://www.ictacademypune.com/pairobochamps/application-form/"
+                                    class="menu-link"
+                                    >Registration</a
+                                  >
+                                </li>
+                              </ul>
+                            </div>
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
    <!-- Include Bootstrap JS and jQuery if needed -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
