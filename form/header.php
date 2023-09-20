33c740241018867b3f3a8422ff80ccd2a7c247cf
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-09-20 17:52:08 +0530

    updated header

--- form/header.php
+++ form/header.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 
-<img src="header.png" alt="header image">
+<img src="header.png" alt="header_image">
    <!-- Include Bootstrap JS and jQuery if needed -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
